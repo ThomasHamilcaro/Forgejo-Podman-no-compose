fde48e6bb883598bf5015e5b113a4c549d08be13
Thomas 2 ERT <thomas.hamilcaro@ert-energy.com>
2025-10-17 09:13:54 +0200

    [Forgejo] - Montée de version vers la latest

--- start_forgejo.sh
+++ start_forgejo.sh
@@ -5,7 +5,7 @@
 ## docker-compose.yml permet une automatisation des paramètres run plus bas dès le fichier de configuration.
 
 # Forgejo version to use
-version=12.0.4 #EOL à date du 16/10/2025, date de la MAJ, en attente du nouvelle MAJ
+version=13.0.0
 
 # Run the Forgejo container with Podman (ensure Podman is installed and configured)
 sudo podman run -d \ #sudo car on ne prend pas la version rootless
